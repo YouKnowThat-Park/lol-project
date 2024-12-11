f61211d7a1ffcb208d98f225c6ca6165b6c2c4e1
wooseok <youkn0wthat@naver.com>
2024-12-11 15:59:30 +0900

    ✨ :feat: champ,item 타입 설정 🤡

---
+++ src/app/types/champions.ts
@@ -0,0 +1,17 @@
+// 챔피언 데이터 & 이미지
+interface ChampionData {
+  id: string;
+  key: number;
+  title: string;
+  name: string;
+  blurb: string;
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+}
---
+++ src/app/types/items.ts
@@ -0,0 +1,21 @@
+// 아이템 & 이미지 & 가격
+interface ChampItem {
+  name: string;
+  description: string;
+  plaintext: string;
+  image: {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+  };
+  gold: {
+    base: number;
+    purchasable: boolean;
+    total: number;
+    sell: number;
+  };
+}
