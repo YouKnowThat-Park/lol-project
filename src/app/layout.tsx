d2f5b829f0bf174fc8b82dcfb6b0fc5980ab3387
wooseok <youkn0wthat@naver.com>
2024-12-11 12:54:05 +0900

    ✨ :feat: 레이아웃에 헤더 틀 잡음 😊

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,8 @@
+import "./globals.css";
+import PlayLogo from "../app/assets/playImg.png";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,7 +11,48 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div>안녕하세요</div>
+        <div className="flex justify-center h-20 bg-black space-x-20 items-center">
+          <div>
+            <Image
+              src="/assets/logoImg.png"
+              alt="logoImg"
+              width={120}
+              height={0}
+            />
+          </div>
+          <nav
+            className="flex justify-center space-x-20 items-center"
+            style={{ color: "#C8AA64" }}
+          >
+            <div>
+              <p className="ml-1.5">OP.GG</p>
+              <p>전적 검색</p>
+            </div>
+            <div>
+              <p className="ml-1.5">롤 패치</p>
+              <p>공지사항</p>
+            </div>
+            <div className="space-y-1.5">
+              <p>로테이션</p>
+              <div className="w-16 h-px bg-white" />
+              <p className="ml-1.5">챔피언</p>
+            </div>
+          </nav>
+
+          <nav className="flex justify-center text-white">
+            <p>공지사항</p>
+            <p>커뮤니티</p>
+            <a href="https://www.leagueoflegends.com/ko-kr/" target="_blank">
+              <Image
+                className="ml-12"
+                src="/assets/playImg.png"
+                alt="playImg"
+                width={90}
+                height={40}
+              />
+            </a>
+          </nav>
+        </div>
         {children}
       </body>
     </html>
