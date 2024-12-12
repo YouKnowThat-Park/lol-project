85c716d018cc655448e383e565c2e2ad3d5c7e10
wooseok <youkn0wthat@naver.com>
2024-12-13 01:41:21 +0900

    ✨ :feat: 다크모드 함수

---
+++ src/components/useDarkMode.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function useDarkMode() {
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") as "light" | "dark";
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
+  return { toggleTheme, theme };
+}
