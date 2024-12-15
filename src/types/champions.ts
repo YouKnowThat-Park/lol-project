f5d74810ba743348c3e287889f9a0cb4790bc691
wooseok <youkn0wthat@naver.com>
2024-12-16 02:04:39 +0900

    ✨ :feat: 챔피언 디테일 페이지 타입 정의

--- src/types/champions.ts
+++ src/types/champions.ts
@@ -15,3 +15,46 @@ interface ChampionData {
     h: number;
   };
 }
+
+interface ChampionSkill {
+  id: string;
+  name: string;
+  description: string;
+  tooltip: string;
+  cooldown: number[];
+  cost: number[];
+  range: number[];
+  image: {
+    full: string;
+  };
+}
+
+interface ChampionDetailType {
+  id: string;
+  key: string;
+  name: string;
+  title: string;
+  blurb: string;
+  image: {
+    full: string;
+  };
+  spells: {
+    id: string;
+    name: string;
+    description: string;
+    tooltip: string;
+    cooldown: number[];
+    cost: number[];
+    range: number[];
+    image: {
+      full: string;
+    };
+  }[];
+  passive: {
+    name: string;
+    description: string;
+    image: {
+      full: string;
+    };
+  };
+}
