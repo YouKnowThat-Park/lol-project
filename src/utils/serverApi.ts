40c256915feb439b7cbf07b04688082f23c740d5
wooseok <youkn0wthat@naver.com>
2024-12-16 01:50:17 +0900

    ✨ :feat: 챔피언 디테일 페이지 정보 가져오기

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -31,6 +31,25 @@ export default async function ChampionDataFetch() {
   };
 }
 
+//챔피언 디테일 정보 가져오기
+export async function ChampionDetailFetch(
+  championId: string
+): Promise<{ data: ChampionDetailType; version: string }> {
+  const latestVersion = await fetchLatestVersion();
+  const detailRes = await fetch(
+    `https://ddragon.leagueoflegends.com/cdn/${latestVersion}/data/ko_KR/champion/${championId}.json`
+  );
+  if (!detailRes) {
+    throw new Error("챔피언 상세정보를 불러오지 못했습니다.");
+  }
+  const championDetail = await detailRes.json();
+
+  return {
+    data: championDetail.data[championId] as ChampionDetailType,
+    version: latestVersion,
+  };
+}
+
 // 아이템 정보보 가져오기기
 export async function ItemsDataFetch() {
   const latestVersion = await fetchLatestVersion();
@@ -48,3 +67,5 @@ export async function ItemsDataFetch() {
     version: latestVersion,
   };
 }
+
+//아이템 디테일 정보 가져오기
