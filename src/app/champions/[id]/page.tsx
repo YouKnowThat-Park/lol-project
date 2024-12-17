0efc00611db8ff8d7b78ef6217a1b31736719e1b
wooseok <youkn0wthat@naver.com>
2024-12-17 22:02:52 +0900

    🛠 :Refactoring: 챔피언 디테일 페이지 컴포넌트 분리 완료료

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,5 +1,7 @@
+import ChampionDetail_info from "@/components/ChampionDetail/Detail_info";
+import Detail_spell from "@/components/ChampionDetail/Detail_skills";
+import Detail_skins from "@/components/ChampionDetail/Detail_skins";
 import { ChampionDetailFetch } from "@/utils/serverApi";
-import Image from "next/image";
 
 interface ChampionDetailProps {
   params: {
@@ -16,94 +18,23 @@ export default async function ChampionPage({ params }: ChampionDetailProps) {
   }
 
   return (
-    <div className="flex justify-center py-8 max-h-screen mt-[120px]">
-      <div className="p-6 bg-gray-800 text-white rounded-lg shadow-lg w-[700px] h-auto ">
+    <div className="flex justify-center py-8 mt-[120px]  ">
+      <div className="p-6 bg-gray-800 text-white rounded-lg shadow-lg w-[700px] flex-wrap object-cover ">
         {/* 챔피언 정보 */}
-        <div className="flex items-center gap-4 mb-6">
-          <Image
-            src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/${champion.image.full}`}
-            alt={champion.name}
-            width={120}
-            height={120}
-            className="rounded-md border border-gray-600"
-          />
-          <div>
-            <h1 className="text-4xl font-bold text-white">{champion.name}</h1>
-            <h2 className="text-xl text-gray-400">{champion.title}</h2>
-            <h3>{champion.blurb}</h3>
-          </div>
-        </div>
+        <ChampionDetail_info champion={champion} version={version} />
 
-        {/* 스킬 정보 */}
         <div className="mt-6">
           <h3 className="text-2xl font-semibold mb-4 border-b border-gray-600 pb-2 text-center">
             스킬 정보
           </h3>
-          {/* 스킬 목록 */}
-          <ul className="flex gap-6 justify-center">
-            <li className="relative ">
-              <div className="relative w-[80px] h-[80px] group">
-                <Image
-                  src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/passive/${champion.passive.image.full}`}
-                  alt={champion.passive.name}
-                  width={80}
-                  height={80}
-                  className="rounded-md border border-gray-600 object-cover"
-                />
-                <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] h-[250px] bg-white rounded-lg shadow-2xl flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-                  <p className="text-gray-800 text-sm text-center px-4">
-                    {champion.passive.description}
-                  </p>
-                </div>
-              </div>
-              <div className="text-center mt-2">
-                <strong className="text-sm">Passive:</strong>
-                <span className="block text-xs">{champion.passive.name}</span>
-              </div>
-            </li>
 
-            {/* Q, W, E, R 스킬 */}
-            {champion.spells.map((spell, index) => (
-              <li key={index} className="relative">
-                <div className="relative w-[80px] h-[80px] group ">
-                  <Image
-                    src={`https://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${spell.image.full}`}
-                    alt={spell.name}
-                    width={80}
-                    height={80}
-                    className="rounded-md border border-gray-600 object-cover"
-                  />
-                  <div className="pointer-events-none absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-[350px] h-[250px] bg-white rounded-lg shadow-2xl flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10">
-                    <p className="text-gray-800 text-sm text-center px-4">
-                      {spell.description}
-                    </p>
-                  </div>
-                </div>
-                <div className="text-center mt-2">
-                  <strong className="text-sm">
-                    {["Q", "W", "E", "R"][index]}:
-                  </strong>
-                  <span className="block text-xs">{spell.name}</span>
-                </div>
-              </li>
-            ))}
-          </ul>
+          {/* 스킬 목록 */}
+          <Detail_spell champion={champion} version={version} />
+
           <h3 className="text-2xl font-semibold mb-4 border-b border-gray-600 pb-2 text-center"></h3>
+
           {/*챔피언 스킨*/}
-          <ul className="flex flex-wrap justify-center gap-3 mt-10">
-            {champion.skins.map((skins, index) => (
-              <li key={index} className="flex justify-center">
-                <div className="flex justify-center">
-                  <Image
-                    src={`https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champion.id}_${skins.num}.jpg`}
-                    alt={skins.name}
-                    height={150}
-                    width={150}
-                  />
-                </div>
-              </li>
-            ))}
-          </ul>
+          <Detail_skins champion={champion} />
         </div>
       </div>
     </div>
