a2d10aea732858f2aedb0741198068d138ddf355
wooseok <youkn0wthat@naver.com>
2024-12-17 09:55:29 +0900

    😗 :edit: 챔피언 관리 타입 수정 (이유 : 챔피언 디테일 페이지 에서 새롭게 참고해서 보여줄 타입 추가)

--- src/types/champions.ts
+++ src/types/champions.ts
@@ -28,6 +28,12 @@ interface ChampionSkill {
     full: string;
   };
 }
+interface ChampionSkin {
+  id: string; // 스킨 ID
+  num: number; // 스킨 번호
+  name: string; // 스킨 이름
+  chromas: boolean; // 크로마 지원 여부
+}
 
 interface ChampionDetailType {
   id: string;
@@ -38,6 +44,7 @@ interface ChampionDetailType {
   image: {
     full: string;
   };
+  skins: ChampionSkin[]; // 스킨 데이터
   spells: {
     id: string;
     name: string;
@@ -49,12 +56,12 @@ interface ChampionDetailType {
     image: {
       full: string;
     };
-  }[];
+  }[]; // Q, W, E, R 스킬 배열
   passive: {
     name: string;
     description: string;
     image: {
       full: string;
     };
-  };
+  }; // 패시브 스킬 데이터
 }
