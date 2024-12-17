05767eb62ded445986e52bcbe5890739582698e1
wooseok <youkn0wthat@naver.com>
2024-12-17 10:18:38 +0900

    ✨ :feat: useQuery 사용하기 위해 provider 설정정

---
+++ src/app/_provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export default function RotationProvider({ children }: ProvidersProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
