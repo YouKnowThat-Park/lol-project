c860ba2a044fc2e86674899c2d23160717f0a337
wooseok <youkn0wthat@naver.com>
2024-12-17 22:06:15 +0900

    ✨ :feat: 메인 페이지에 비디오 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div className="flex justify-center mt-10 ">^-^)b </div>;
+  return (
+    <video autoPlay muted>
+      <source type="video/webm" />
+      <source
+        type="video/mp4"
+        src="https://cmsassets.rgpub.io/sanity/files/dsfx7636/universe/ae35a3bd6a2712e0d42feea5bdc01ac6d5b70b7f.mp4"
+      />
+    </video>
+  );
 }
