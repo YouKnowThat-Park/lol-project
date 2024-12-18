c8bbfcab7e7d156ac0d9a5fdb00df74bd2249efc
wooseok <youkn0wthat@naver.com>
2024-12-18 23:16:31 +0900

    🍺 :edit:  Isr 랜더링 방식으로 수정정

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,24 +1,16 @@
-"use client";
+// src/app/champions/page.tsx
 
-import ChampionData_item from "@/components/ChampionData/Data_info";
 import { ChampionData } from "@/types/champions";
 import ChampionDataFetch from "@/utils/serverApi";
-import { useEffect, useState } from "react";
+import ChampionData_item from "@/components/ChampionData/Data_info";
 
-export default function Champions() {
-  const [data, setData] = useState<Record<string, ChampionData> | null>(null);
-  const [latestVersion, setLatestVersion] = useState<string>("");
+export const revalidate = 86400; // 하루마다 재검증
 
-  useEffect(() => {
-    async function fetchData() {
-      const championData = await ChampionDataFetch();
-      setData(championData.data);
-      setLatestVersion(championData.version);
-    }
-    fetchData();
-  }, []);
+export default async function Champions() {
+  const championData = await ChampionDataFetch();
 
-  if (!data) return <div>loading...</div>;
+  // Object.values()의 반환 타입을 ChampionData[]로 명시적으로 지정
+  const champions = Object.values(championData.data) as ChampionData[];
 
   return (
     <div className="p-8 bg-gradient-to-b min-h-screen text-white">
@@ -26,11 +18,11 @@ export default function Champions() {
         Champions
       </h1>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-6">
-        {Object.values(data).map((champion: ChampionData) => (
+        {champions.map((champion) => (
           <ChampionData_item
             key={champion.id}
             champion={champion}
-            latestVersion={latestVersion}
+            latestVersion={championData.version}
           />
         ))}
       </ul>
